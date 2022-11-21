d57acbde8f05c1ff60b46c1dfbad03d65fd32291
Gus Becker <60982210+cgusb@users.noreply.github.com>
2022-11-21 18:42:01 -0500

    Initial commit

---
+++ js/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400)
+  resetSketch()
+  console.log('Press SPACE to stop looping or r to reset.')
+}
+
+function draw() {
+  background(220)
+}
+
+function resetSketch() {
+  background(220)
+}
