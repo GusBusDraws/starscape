300539359789e6772b8c20e06eb204acbd9e53c2
cgusb <cgusbecker@gmail.com>
2022-11-30 21:57:51 -0500

    Add nebula color

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,15 @@
 let N_STARS = 100;
 let MIN_CLUSTER_R = 10;
 let MAX_CLUSTER_R = 250;
-let DEBUG_MODE = false;
-let N_NEBULA_STARS = 1000;
+let DEBUG_MODE = true;
+let N_NEBULA_STARS = 10000;
+let COLORS = ['#5f0f40', '#9a031e', '#fb8b24', '#e36414', '#0f4c5c']
 let y1;
 let y2;
 let a;
 let b;
-let c = 0.02;
-let d = 0.5;
+let c;
+let d;
 
 function setup() {
   createCanvas(500, 500);
@@ -18,11 +19,17 @@ function setup() {
 
 function draw() {
   noLoop();
+  let bg_color = color(0)
+  // index = COLORS.indexOf(bg_color)
+  // COLORS.splice(index, 1)
+  background(bg_color);
   let wave1Y = [];
   let wave2Y = [];
   for (let x = 0; x < width; x += 0.1) {
-    a = x;
-    b = height / 10;
+    a = 0.6 * height;
+    b = 0.5 * height;
+    c = 0.005;
+    d = 1.25
     y1 = a + b * sin(c * x + d);
     wave1Y.push(y1)
     y2 = a + b * cos(c * x + d);
@@ -37,26 +44,25 @@ function draw() {
       point(wave_i / 10, wave2Y[wave_i]);
     }
   }
-  stroke(255)
+  let nebulaColor = random(COLORS)
+  // index = COLORS.indexOf(nebulaColor)
+  // COLORS.splice(index, 1)
+  stroke(nebulaColor)
   strokeWeight(1)
   for (i_nebula = 0; i_nebula < N_NEBULA_STARS; i_nebula ++) {
     let xNebula = random(width);
-    a = xNebula;
-    b = height / 10;
     y1 = a + b * sin(c * xNebula + d);
     y2 = a + b * cos(c * xNebula + d);
     let yNebula;
     if (y1 < y2) {
-      // yNebula = (y2 + y1) / 2
-      // yNebula = random(y1, y2)
       yNebula = randomGaussian((y2 + y1) / 2, (y2 - y1) / 4);
     } else {
-      // yNebula = (y1 + y2) / 2
-      // yNebula = random(y2, y1)
       yNebula = randomGaussian((y1 + y2) / 2, (y1 - y2) / 4);
     }
     point(xNebula, yNebula)
   }
+  // filter(BLUR, 1)
+  let starColor = color(255)
   for (let star_i = 0; star_i < N_STARS; star_i ++) {
     // Cluster or singular star
     let clusterRand = random(1);
@@ -75,13 +81,12 @@ function draw() {
           strokeWeight(3);
         }
         // Star brightness (opacity)
-        let opacityRand = random(1);
-        stroke(255, 255, 255, opacityRand * 255)
+        let opacityRand = random(255);
+        starColor.setAlpha(opacityRand)
+        stroke(starColor)
         // Cluster size
         let offsetX = randomGaussian(posX, clusterSize)
         let offsetY = randomGaussian(posY, clusterSize)
-        // let offsetX = randomGaussian(-clusterSize, clusterSize)
-        // let offsetY = randomGaussian(-clusterSize, clusterSize)
         point(offsetX, offsetY);
       }
     } else {
@@ -91,6 +96,5 @@ function draw() {
 }
 
 function resetSketch() {
-  background(0);
   redraw();
 }
